fb059ec1ff79b26e2c860bd43816e213edb94553
cs-ehanc001 <ehanc001@odu.edu>
2023-04-26 13:04:43 -0400

    improve error message

--- main.c
+++ main.c
@@ -5,7 +5,9 @@
 
 int main(int argc, char** argv) {
   if (argc != 2) {
-    puts("Expected an argument");
+    puts("Expected an argument\n");
+    puts("To calculate the <number>th Fibonacci number:");
+    printf("Usage: %s <number>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
